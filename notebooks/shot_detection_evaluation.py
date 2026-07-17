56dfe960ce087e7f66d12397f2ab8c207313adff
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2026-07-17 15:35:48 +0100

    Creat PostShotData class to handle pipeline, and access to intermediate steps and data for model input

--- notebooks/shot_detection_evaluation.py
+++ notebooks/shot_detection_evaluation.py
@@ -11,10 +11,7 @@ with app.setup:
     from plotnine import ggplot, aes, labs, geom_vline, geom_hline, theme_bw
 
     from data_readers import batch_read_events, batch_read_puck_tracking
-    from processing.tracking import calculate_elapsed_time
-    from processing.events import extract_flip, timecode_to_seconds
-    from post_shot.detection import calculate_shot_detection
-    from post_shot.evaluation import evaluate_shot_detection
+    from post_shot.features import PostShotData
 
 
 @app.cell
@@ -26,34 +23,12 @@ def _():
 
 @app.cell(hide_code=True)
 def _(game_id_selector):
-    events = batch_read_events(f"data/{game_id_selector.value}/*_sapifullevents.json").with_columns(extract_flip())
-    shots = (
-        events
-        .with_columns(elapsed_time = timecode_to_seconds())
-        .filter(c('name') == 'shot')
-        .sort(c('game_id', 'period', 'elapsed_time'))
-        .with_row_index(name='shot_id')
-    )
+    events = batch_read_events(f"data/{game_id_selector.value}/*_sapifullevents.json")
 
     puck_tracking = batch_read_puck_tracking(
         f"data/{game_id_selector.value}/HOCKEY_NHL_*_Period_*.parquet",
     )
-
-    puck_tracking = (
-        puck_tracking
-        .with_columns(elapsed_time = calculate_elapsed_time())
-        .filter(c('entity_id') == '1')
-        .sort(c('game_id', 'period', 'elapsed_time'))
-        .drop(c('entity_id', 'clock_state'))
-    )
-    return puck_tracking, shots
-
-
-@app.cell(hide_code=True)
-def _(acc_slider, angle_slider, distance_slider, puck_tracking, shots):
-    shot_features = calculate_shot_detection(shots, puck_tracking, distance_threshold=distance_slider.value, impact_acceleration_threshold=acc_slider.value, deflection_angle_threshold=angle_slider.value)
-    shots_with_features = shots.join(shot_features, on=c('shot_id'), how='left').collect()
-    return (shots_with_features,)
+    return events, puck_tracking
 
 
 @app.cell
@@ -65,9 +40,10 @@ def _():
 
 
 @app.cell
-def _(shots_with_features):
-    metrics = evaluate_shot_detection(shots_with_features)
-    return (metrics,)
+def _(acc_slider, angle_slider, distance_slider, events, puck_tracking):
+    post_shot_data = PostShotData(events, puck_tracking, distance_threshold=distance_slider.value, impact_acceleration_threshold=acc_slider.value, deflection_angle_threshold=angle_slider.value)
+    metrics = post_shot_data.evaluate().collect()
+    return metrics, post_shot_data
 
 
 @app.cell
@@ -76,10 +52,16 @@ def _(acc_slider, angle_slider, distance_slider, metrics):
     return
 
 
+@app.cell
+def _(post_shot_data):
+    full_output = post_shot_data.full_output().collect()
+    return (full_output,)
+
+
 @app.cell(hide_code=True)
-def _(shots_with_features):
+def _(full_output):
     timing_plot = (
-        shots_with_features
+        full_output
         .with_columns(timing_error = c('elapsed_time') - c('shot_time'))
         >> ggplot(aes(x='timing_error'))
         + geom_vline(xintercept=0, linetype='dashed')
@@ -98,9 +80,9 @@ def _(shots_with_features):
 
 
 @app.cell(hide_code=True)
-def _(shots_with_features):
+def _(full_output):
     location_plot = (
-        shots_with_features
+        full_output
         .with_columns(
             x_error = c('x_adj_coord') - c('shot_x'),
             y_error = c('y_adj_coord') - c('shot_y')
--- scripts/post_shot/features.py
+++ scripts/post_shot/features.py
@@ -1,12 +1,137 @@
+from typing import List
+from dataclasses import dataclass
+from functools import cached_property
+
 import polars as pl
 from polars import col as c
 
 from processing.events import timecode_to_seconds, extract_flip
 from processing.tracking import calculate_elapsed_time
 from post_shot.detection import calculate_shot_detection
+from post_shot.evaluation import evaluate_shot_detection
 from utils import distance_to_point_2d
 
-def shot_features() -> pl.Expr:
+@dataclass
+class PostShotData:
+    """
+    Class to handle post-shot data processing, including shot detection, feature extraction, and evaluation.
+    Takes in events and puck tracking data, and provides methods to prepare the data, detect shots, extract features,
+    and evaluate the shot detection algorithm.
+
+    window_size: float, optional
+        The size of the window (in seconds) to select tracking data for each shot. Default is 1.6 seconds.
+    distance_threshold: float, optional
+        The maximum distance (in feet) from the shot location to consider tracking data for the shot.
+    impact_acceleration_threshold: float, optional
+        The threshold for the goal acceleration to consider the shot as having been blocked/saved/deflected.
+    deflection_angle_threshold: float, optional
+        The threshold for the change in angle to consider the shot as having been deflected.
+    """
+
+    events: pl.LazyFrame
+    puck_tracking: pl.LazyFrame
+    window_size: float = 1.6
+    distance_threshold: float = 8
+    impact_acceleration_threshold: float = -800
+    deflection_angle_threshold: float = 25
+
+    @cached_property
+    def shots(self) -> pl.LazyFrame:
+        return (
+            self.events
+            .with_columns(
+                extract_flip(),
+                elapsed_time = timecode_to_seconds()
+            )
+            .filter(c('name') == 'shot')
+            .sort(c('game_id'), c('period'), c('elapsed_time'))
+            .with_row_index(name='shot_id')
+        )
+
+    @cached_property
+    def puck_tracking_prepared(self) -> pl.LazyFrame:
+        return (
+            self.puck_tracking
+            .with_columns(elapsed_time = calculate_elapsed_time())
+            .filter(
+                c('entity_id') == '1'
+            )
+            .drop(c('entity_id'), c('clock_state'))
+        )
+
+    def prepare_shots(self) -> pl.LazyFrame:
+        """
+        Prepares the shots dataframe by filtering for shot events and adding necessary columns.
+        Returns a LazyFrame with shot events and their corresponding shot_id.
+        """
+        return self.shots
+    
+    def prepare_puck_tracking(self) -> pl.LazyFrame:
+        """
+        Prepares the puck tracking dataframe by filtering for puck tracking data and adding necessary columns.
+        Returns a LazyFrame with puck tracking data.
+        """
+        return self.puck_tracking_prepared
+    
+    def detect_shots(self) -> pl.LazyFrame:
+        """
+        Detects shots by joining the shots dataframe with the puck tracking dataframe and calculating detection results.
+        """
+        return calculate_shot_detection(
+            self.shots,
+            self.puck_tracking_prepared,
+            window_size=self.window_size,
+            distance_threshold=self.distance_threshold,
+            impact_acceleration_threshold=self.impact_acceleration_threshold,
+            deflection_angle_threshold=self.deflection_angle_threshold
+        )
+    
+    def with_features(self) -> pl.LazyFrame:
+        """
+        Returns a LazyFrame with detected shots and their corresponding post-shot features.
+        """
+        return self.detect_shots().with_columns(shot_features())
+    
+    def evaluate(self) -> pl.LazyFrame:
+        return evaluate_shot_detection(self.full_output())
+    
+    def full_output(self) -> pl.LazyFrame:
+        """
+        Returns a LazyFrame with all relevant shot information, including shot details, detection results, and post-shot features.
+        """
+
+        detected = self.with_features()
+
+        return (
+            self.shots
+            .select(
+                c('game_id'), c('period'), c('game_time'), c('elapsed_time'), c('shot_id'),
+                c('team'), c('player_first_name'), c('player_last_name'), c('player_reference_id'),
+                c('x_adj_coord'), c('y_adj_coord'), c('expected_goals_all_shots').alias('pre_shot_xg'),
+                c('type'), c('outcome'), c('flags')
+            ).join(
+                detected,
+                on='shot_id',
+                how='left'
+            )
+        )
+    
+    def model_input(self) -> pl.LazyFrame:
+        """
+        Returns a LazyFrame with the necessary features for model input, including shot speed, on-goal status, and distances to various goal locations.
+        Only includes shots that were detected and are from the offensive zone (x >= 25).
+        """
+        return (
+            self.with_features()
+            .drop_nulls(c('shot_speed')) # Only keep shots with valid speed (i.e. shots that were detected)
+            .filter(
+                c('shot_x') >= 25 # Only o-zone shots
+            )
+            .select(c('shot_id', 'shot_speed', 'on_goal', 'dist_to_post', 'dist_to_crossbar', 'dist_to_top_corner', 'dist_to_center'))
+        )
+
+
+def shot_features() -> List[pl.Expr]:
     """
     Returns a list of expressions with post-shot features.
     Should be applied to output of post_shot.detection.calculate_shot_detection()
@@ -32,53 +157,3 @@ def shot_features() -> pl.Expr:
         dist_to_center.alias('dist_to_center'),
         nearest_post_y.alias('nearest_post_y')
     ]
-
-def pipeline(
-    events: pl.DataFrame | pl.LazyFrame,
-    puck_tracking: pl.DataFrame | pl.LazyFrame,
-    window_size: int = 1.6
-) -> pl.DataFrame | pl.LazyFrame:
-    """
-    Full pipeline for post-shot model input. Takes in events and puck tracking data, without transformations
-    (i.e. output of data_readers functions), and returns a dataframe with post-shot features.
-
-    window_size: float, optional
-        See post_shot.detection.calculate_shot_detection for details.
-    """
-
-    shots = (
-        events
-        .with_columns(
-            extract_flip(),
-            elapsed_time = timecode_to_seconds()
-        )
-        .filter(c('name') == 'shot')
-        .sort(c('game_id', 'period', 'elapsed_time'))
-        .with_row_index(name='shot_id')
-    )
-
-    puck_tracking = (
-        puck_tracking
-        .with_columns(elapsed_time = calculate_elapsed_time())
-        .filter(
-            c('entity_id') == '1'
-        )
-        .drop(c('entity_id', 'clock_state'))
-    )
-    
-    shots_with_features = (
-        calculate_shot_detection(shots, puck_tracking, window_size=window_size)
-        .with_columns(shot_features())
-        .drop_nulls(c('shot_speed'))
-        .filter(
-            c('shot_speed').is_not_null(), # Only shots where detection was successful
-            c('shot_x') >= 25 # Only o-zone shots
-        )
-        .select(c('shot_id', 'shot_speed', 'on_goal', 'dist_to_post', 'dist_to_crossbar', 'dist_to_top_corner', 'dist_to_center'))
-    )
-
-    return (
-        shots
-        .select(c('shot_id'), c('expected_goals_all_shots').alias('pre_shot_xg'))
-        .join(shots_with_features, on='shot_id', how='right')
-    )
