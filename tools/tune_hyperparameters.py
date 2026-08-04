f89e45fcd9392a3c8bec3672fff3b68b0386f850
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2026-08-04 11:25:38 +0000

    Create experiment runner CLI tool

---
+++ tools/tune_hyperparameters.py
@@ -0,0 +1,87 @@
+import argparse
+
+import polars as pl
+import optuna
+
+from scripts.data_readers import batch_read_shot_data
+from scripts.models.experiments import ExperimentRunner
+from scripts.models.data import prepare_data
+from scripts.models.features import pre_shot_features
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        prog="model_experiments",
+        description="Run model experiments with hyperparameter tuning using Optuna.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        "output_file",
+        type=str,
+        help="Output file to save experiment results."
+    )
+
+    parser.add_argument(
+        "--data_pattern",
+        type=str,
+        default="/output/shot_data/20242025/*.parquet",
+        help="Pattern to read data files."
+    )
+
+    parser.add_argument(
+        "--split_path",
+        type=str,
+        default=None,
+        help="Path to the train/test split file."
+    )
+
+    parser.add_argument(
+        "--frameworks",
+        nargs='+',
+        default=['xgboost-dart', 'lightgbm-dart', 'lightgbm', 'xgboost'],
+        help="List of frameworks to test."
+    )
+
+    parser.add_argument(
+        "--strategies",
+        nargs='+',
+        default=['RO', 'RU', 'WCE'],
+        help="List of imbalance strategies to test."
+    )
+
+    parser.add_argument(
+        "--n_trials",
+        type=int,
+        default=50,
+        help="Number of trials for hyperparameter tuning."
+    )
+
+    parser.add_argument(
+        "--multivariate",
+        action='store_true',
+        help="Use multivariate sampling for hyperparameter tuning."
+    )
+
+    parser.add_argument(
+        "--info_log",
+        action='store_true',
+        help="Enable info logging."
+    )
+
+    args = parser.parse_args()
+
+    data = batch_read_shot_data(args.data_pattern).pipe(prepare_data)
+    experiment = ExperimentRunner(data.collect(), pre_shot_features, "goal", split_path=args.split_path)
+
+    if args.info_log:
+        optuna.logging.set_verbosity(optuna.logging.INFO)
+    else:
+        optuna.logging.set_verbosity(optuna.logging.WARNING)
+
+    results = experiment.run_all(frameworks=args.frameworks, strategies=args.strategies, n_trials=args.n_trials, multivariate=args.multivariate)
+    pl.from_dicts(results).write_parquet(args.output_file)
+
+if __name__ == "__main__":
+    main()
