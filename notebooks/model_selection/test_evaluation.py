8711396d0f2208d6f490db9730d791420c365669
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2026-08-21 14:48:46 +0000

    Add imputation model for post-shot xG

--- notebooks/model_selection/test_evaluation.py
+++ notebooks/model_selection/test_evaluation.py
@@ -20,9 +20,9 @@ with app.setup:
 
 @app.cell
 def _():
-    shot_data = batch_read_shot_data("/output/shot_data/20252026/*.parquet")
-    pre_shot_xg = pl.scan_parquet("/output/predictions/20252026/pre_shot_1308.parquet")
-    post_shot_xg = pl.scan_parquet("/output/predictions/20252026/post_shot_1308.parquet")
+    shot_data = batch_read_shot_data("/output/shot_data/20252026-clean/*.parquet")
+    pre_shot_xg = pl.scan_parquet("/output/predictions/20252026/pre_shot_2008.parquet")
+    post_shot_xg = pl.scan_parquet("/output/predictions/20252026/post_shot_2108.parquet")
     return post_shot_xg, pre_shot_xg, shot_data
 
 
@@ -41,8 +41,12 @@ def _(post_shot_xg, pre_shot_xg, shot_data):
             on=["game_id", "period", "shot_id"],
             how='left',
             validate='1:1'
-        ).rename({'pre_shot_xg':'sportlogiq_xg'})
-        .with_columns(pl.when(c('type').str.contains('blocked')).then(0).otherwise(c('post_shot')).alias('post_shot'))
+        )
+        .with_columns(
+            pl.when(c('type').str.contains('blocked')).then(0).otherwise(c('post_shot')).alias('post_shot'),
+        ).with_columns(
+            c('post_shot').fill_null(c('pre_shot')).alias('post_shot_imputed')
+        )
         # .select(c('game_id', 'period', 'shot_id', 'goal', 'sportlogiq_xg', 'pre_shot', 'post_shot'))
         .collect()
     )
@@ -51,64 +55,54 @@ def _(post_shot_xg, pre_shot_xg, shot_data):
 
 @app.function
 def get_predictions(data: pl.DataFrame):
-    base_rate = data.select(c('goal')).to_series().mean()
     goals = data.select(c('goal')).to_series().to_list()
     sportlogiq = data.select(c('sportlogiq_xg')).to_series().to_list()
     pre_shot = data.select(c('pre_shot')).to_series().to_list()
     post_shot = data.select(c('post_shot')).to_series().to_list()
+    post_shot_imputed = data.select(c('post_shot_imputed')).to_series().to_list()
 
-    return goals, base_rate, sportlogiq, pre_shot, post_shot
+    return goals, sportlogiq, pre_shot, post_shot, post_shot_imputed
 
 
 @app.cell
 def _(data):
     unblocked = data.filter(c('post_shot').is_not_null(), c('type').str.contains('blocked').not_())
 
-    goals, base_rate, sportlogiq, pre_shot, _ = get_predictions(data)
-    unblocked_goals, unblocked_base_rate, unblocked_sportlogiq, unblocked_pre_shot, unblocked_post_shot = get_predictions(unblocked)
+    goals, sportlogiq, pre_shot, _, post_shot_imputed = get_predictions(data)
+
+    post_shot_valid = data.filter(c('post_shot').is_not_null())
+    post_shot = post_shot_valid.select(c('post_shot')).to_series().to_list()
+    goals_post_shot = post_shot_valid.select(c('goal')).to_series().to_list()
 
 
     model_metrics = pl.DataFrame({
-        'shots': ['All Shots', 'All Shots', 'All Shots',
-                  'Unblocked Shots', 'Unblocked Shots', 'Unblocked Shots', 'Unblocked Shots'],
-        'model': ['Random', 'Sportlogiq', 'Pre-Shot', 'Random', 'Sportlogiq', 'Pre-Shot', 'Post-Shot'],
+        'model': ['Sportlogiq', 'Pre-Shot', 'Post-Shot', 'Post-Shot (Imputed)'],
         'prauc': [
-            base_rate,
             average_precision_score(goals, sportlogiq),
             average_precision_score(goals, pre_shot),
-            unblocked_base_rate,
-            average_precision_score(unblocked_goals, unblocked_sportlogiq),
-            average_precision_score(unblocked_goals, unblocked_pre_shot),
-            average_precision_score(unblocked_goals, unblocked_post_shot),
+            average_precision_score(goals_post_shot, post_shot),
+            average_precision_score(goals, post_shot_imputed),
         ],
         'rocauc': [
-            0.5,
             roc_auc_score(goals, sportlogiq),
             roc_auc_score(goals, pre_shot),
-            0.5,
-            roc_auc_score(unblocked_goals, unblocked_sportlogiq),
-            roc_auc_score(unblocked_goals, unblocked_pre_shot),
-            roc_auc_score(unblocked_goals, unblocked_post_shot),
+            roc_auc_score(goals_post_shot, post_shot),
+            roc_auc_score(goals, post_shot_imputed),
         ],
         'ece': [
-            None,
             classwise_ece(sportlogiq, goals, method='frequency'),
             classwise_ece(pre_shot, goals, method='frequency'),
-            None,
-            None,
-            None,
-            classwise_ece(unblocked_post_shot, unblocked_goals, method='frequency'),
+            classwise_ece(post_shot, goals_post_shot, method='frequency'),
+            classwise_ece(post_shot_imputed, goals, method='frequency'),
         ]
     })
     return (
-        base_rate,
         goals,
+        goals_post_shot,
         model_metrics,
+        post_shot,
         pre_shot,
         sportlogiq,
-        unblocked_base_rate,
-        unblocked_goals,
-        unblocked_post_shot,
     )
 
 
@@ -118,10 +112,11 @@ def _(model_metrics):
         GT(model_metrics)
         .tab_header(title="Model Evaluation Metrics")
         .tab_source_note("Scores are evaluated on test set (2025-26 Season)")
-        .cols_label(shots="Shots", model="Model", prauc="PR-AUC", rocauc="ROC-AUC", ece="ECE")
-        .tab_stub(rowname_col='model', groupname_col='shots')
+        .tab_source_note("Post-Shot models set blocked shots to 0")
+        .cols_label(model="Model", prauc="PR-AUC", rocauc="ROC-AUC", ece="ECE")
+        .tab_stub(rowname_col='model')
         .sub_missing()
-        .fmt_number(['prauc', 'rocauc', 'ece'], decimals=3)
+        .fmt_number(['prauc', 'rocauc', 'ece'], decimals=4)
     )
 
     metrics_table
@@ -130,9 +125,9 @@ def _(model_metrics):
 
 
 @app.cell
-def _(goals, pre_shot, sportlogiq, unblocked_goals, unblocked_post_shot):
+def _(goals, goals_post_shot, post_shot, pre_shot, sportlogiq):
     pre_precision, pre_recall, _ = precision_recall_curve(goals, pre_shot, drop_intermediate=True)
-    post_precision, post_recall, _ = precision_recall_curve(unblocked_goals, unblocked_post_shot, drop_intermediate=True)
+    post_precision, post_recall, _ = precision_recall_curve(goals_post_shot, post_shot, drop_intermediate=True)
     sportlogiq_precision, sportlogiq_recall, _ = precision_recall_curve(goals, sportlogiq, drop_intermediate=True)
     return (
         post_precision,
@@ -145,30 +140,27 @@ def _(goals, pre_shot, sportlogiq, unblocked_goals, unblocked_post_shot):
 
 
 @app.cell
-def _(goals, pre_shot, sportlogiq, unblocked_goals, unblocked_post_shot):
+def _(goals, goals_post_shot, post_shot, pre_shot, sportlogiq):
     pre_fpr, pre_tpr, __ = roc_curve(goals, pre_shot, drop_intermediate=True)
-    post_fpr, post_tpr, __ = roc_curve(unblocked_goals, unblocked_post_shot, drop_intermediate=True)
+    post_fpr, post_tpr, __ = roc_curve(goals_post_shot, post_shot, drop_intermediate=True)
     sportlogiq_fpr, sportlogiq_tpr, __ = roc_curve(goals, sportlogiq, drop_intermediate=True)
     return post_fpr, post_tpr, pre_fpr, pre_tpr, sportlogiq_fpr, sportlogiq_tpr
 
 
 @app.cell
 def _(
-    base_rate,
     post_precision,
     post_recall,
     pre_precision,
     pre_recall,
     sportlogiq_precision,
     sportlogiq_recall,
-    unblocked_base_rate,
 ):
     df_pre = pl.DataFrame({
         'model': 'Pre-Shot',
         'data': 'All Shots',
         'precision': pre_precision,
         'recall': pre_recall,
-        'base_rate': base_rate
     })
 
     df_sportlogiq = pl.DataFrame({
@@ -176,7 +168,6 @@ def _(
         'data': 'All Shots',
         'precision': sportlogiq_precision,
         'recall': sportlogiq_recall,
-        'base_rate': base_rate
     })
 
     df_post = pl.DataFrame({
@@ -184,7 +175,6 @@ def _(
         'data': 'Unblocked Shots',
         'precision': post_precision,
         'recall': post_recall,
-        'base_rate': unblocked_base_rate
     })
 
     pr_df = pl.concat([df_pre, df_sportlogiq, df_post])
@@ -192,7 +182,6 @@ def _(
     pr_plot = (
         ggplot(pr_df, aes(x='recall', y='precision', color='model'))
         + geom_line()
-        + p9.geom_hline(aes(yintercept='base_rate', linetype='data'))
         + theme_bw(base_size=10)
         + p9.xlim(0,1)
         + p9.ylim(0,1)
@@ -245,9 +234,9 @@ def _(post_fpr, post_tpr, pre_fpr, pre_tpr, sportlogiq_fpr, sportlogiq_tpr):
 
 
 @app.cell
-def _(goals, pre_shot, sportlogiq, unblocked_goals, unblocked_post_shot):
+def _(goals, goals_post_shot, post_shot, pre_shot, sportlogiq):
     pre_prob_true, pre_prob_pred = calibration_curve(goals, pre_shot, n_bins=6)
-    post_prob_true, post_prob_pred = calibration_curve(unblocked_goals, unblocked_post_shot, n_bins=6)
+    post_prob_true, post_prob_pred = calibration_curve(goals_post_shot, post_shot, n_bins=6)
     sportlogiq_prob_true, sportlogiq_prob_pred = calibration_curve(goals, sportlogiq)
 
     df_pre_cal = pl.DataFrame({
