8055ecd6775bd6b0071294ab7f4d30738489c6eb
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2026-08-26 12:31:06 +0000

    Fix to not fail silently

--- tools/run_pipeline.sh
+++ tools/run_pipeline.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+set -e
 
 # Define the start and end dates for each month
 dates=(
-  "2024-10-01 2024-10-31"
-  "2024-11-01 2024-11-30"
-  "2024-12-01 2024-12-31"
-  "2025-01-01 2025-01-31"
-  "2025-02-01 2025-02-28"
-  "2025-03-01 2025-03-31"
-  "2025-04-01 2025-04-30"
-  "2025-05-01 2025-05-31"
-  "2025-06-01 2025-06-30"
+  "2025-10-01 2025-10-31"
+  "2025-11-01 2025-11-30"
+  "2025-12-01 2025-12-31"
+  "2026-01-01 2026-01-31"
+  "2026-02-01 2026-02-28"
+  "2026-03-01 2026-03-31"
+  "2026-04-01 2026-04-30"
+  "2026-05-01 2026-05-31"
+  "2026-06-01 2026-06-30"
 )
 
 # Loop through and run the command, stopping on any failure (set -e equivalent)
@@ -20,5 +21,5 @@ for d in "${dates[@]}"; do
     mappings/NHL_20242025_20252026_game_smt_sportlogiq_id_map.csv \
     mappings/NHL_20242025_20252026_player_sportlogiq_id_map.csv \
     --start_date "$1" --end_date "$2" \
-    --output_dir /output/shot_data/20242025-clean/ || break
+    --output_dir /output/shot_data/20252026/
 done
