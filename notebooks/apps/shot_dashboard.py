8ad4666d268bc81908ebc4184e77640bcf0c327c
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2026-09-14 18:47:59 +0000

    Add xG annotations

--- notebooks/apps/shot_dashboard.py
+++ notebooks/apps/shot_dashboard.py
@@ -88,6 +88,9 @@ def _(
 ):
     shot_data = batch_read_shot_data(f"/output/shot_data/*-wide-window/{sportlogiq_id}_shot_data.parquet").with_row_index(offset=1).collect()
 
+    pre_shot_xg = pl.scan_parquet("/output/predictions/*/pre_shot_0809.parquet").filter(c('game_id') == str(sportlogiq_id)).collect()
+    post_shot_xg = pl.scan_parquet("/output/predictions/*/post_shot_one_hot_0809.parquet").filter(c('game_id') == str(sportlogiq_id)).collect()
+
     tracking = (
         batch_read_entity_tracking(f"/data/smtoasis/regular/games/{smt_id}/*_entity_tracking_processed_measurements.parquet", games.lazy())
         .join(
@@ -110,10 +113,27 @@ def _(
 
     shot_data = (
         shot_data
+        .join(
+            pre_shot_xg,
+            on=['game_id', 'period', 'shot_id'],
+            how='left'
+        ).join(
+            post_shot_xg,
+            on=['game_id', 'period', 'shot_id'],
+            how='left'
+        )
         .with_columns(
             c('shot_x').fill_null(c('x_adj_coord')),
             c('shot_y').fill_null(c('y_adj_coord')),
-            c('shot_time').fill_null(c('elapsed_time'))
+            c('shot_time').fill_null(c('elapsed_time')),
+            result = (
+                pl.when(c('goal')).then(pl.lit('Goal'))
+                .when(c('type').str.contains('blocked')).then(pl.lit('Blocked'))
+                .when(c('outcome') == 'successful').then(pl.lit('On Goal'))
+                .otherwise(pl.lit('Missed'))
+            )
+        ).with_columns(
+            details = pl.format("{} {} - {}mph - {}", c('player_first_name'), c('player_last_name'), c('shot_speed').round(1), c('result'))
         )
         .with_columns(
             pl.concat_list(c('opposing_team_goalie_on_ice_ref')),
@@ -202,7 +222,12 @@ def _(c, pl, puck_tracking, shap, shot_data, shot_selector, time):
         .filter(c('shot_id') == shot_id)
         .with_row_index('x_id', offset=1)
     )
+
+    base_value = shot_shap.select(c('base_value').first()).item()
+    details = shot_plot_data.select(c('details').first()).item()
     return (
+        base_value,
+        details,
         period,
         shot_plot_data,
         shot_puck_tracking,
@@ -213,13 +238,30 @@ def _(c, pl, puck_tracking, shap, shot_data, shot_selector, time):
 
 
 @app.cell
+def _(c, shot_plot_data):
+    pre_xg = shot_plot_data.select(c('pre_shot').round(2).first()).item()
+    post_xg = shot_plot_data.select(c('post_shot').round(2).first()).item()
+    if post_xg is None:
+        post_colour = 'grey'
+    elif post_xg > pre_xg:
+        post_colour = 'green'
+    else:
+        post_colour = 'red'
+    return post_colour, post_xg, pre_xg
+
+
+@app.cell
 def _(
     aes,
+    details,
     game_selector,
     geom_net,
     gg,
     ggplot,
     period,
+    post_colour,
+    post_xg,
+    pre_xg,
     shot_plot_data,
     time_remaining,
 ):
@@ -228,6 +270,11 @@ def _(
         + geom_net()
         # + geom_ice()
         + gg.geom_point(size=5)
+        + gg.geom_label(
+            aes(x=-5, y=5, alpha=0.8), label=f"PreXG: {pre_xg}", size=15, show_legend=False
+        ) + gg.geom_label(
+            aes(x=-5, y=4, alpha=0.8), label=f"PostXG: {post_xg}", colour=post_colour, size=15, show_legend=False
+        )
         + gg.scale_x_reverse()
         + gg.coord_fixed(xlim=(6,-6), ylim=(0, 6))
         + gg.theme_538()
@@ -236,9 +283,10 @@ def _(
             axis_title=gg.element_blank(),
             plot_title=gg.element_text(size=20, weight="bold"),
             dpi=300
-        ) + gg.labs(
+        )
+        + gg.labs(
             title=game_selector.value,
-            subtitle=f"Period {period}, {time_remaining} remaining"
+            subtitle=f"Period {period}, {time_remaining} remaining \n {details}"
         )
     )
     return (goal_plot,)
@@ -280,22 +328,12 @@ def _(
 
 
 @app.cell
-def _(aes, c, gg, ggplot, ml, shot_shap):
+def _(aes, base_value, c, gg, ggplot, ml, shot_shap):
     if shot_shap.height > 0:
         max_shap = shot_shap.select(c('end').max()).item()
-    
+
         shap_plot = (
             ggplot(shot_shap)
-            + gg.geom_rect(
-                aes(
-                    xmin="x_id - 0.4", 
-                    xmax="x_id + 0.4", 
-                    ymin="start", 
-                    ymax="end", 
-                    fill="increase"
-                ),
-                size=0.5
-            )
             + gg.geom_segment(
                 aes(
                     x="x_id - 1.4", 
@@ -306,7 +344,19 @@ def _(aes, c, gg, ggplot, ml, shot_shap):
                 linetype="dashed", 
                 color="gray",
                 inherit_aes=False
-            ) + gg.geom_label(
+            )
+            + gg.geom_rect(
+                aes(
+                    xmin="x_id - 0.4", 
+                    xmax="x_id + 0.4", 
+                    ymin="start", 
+                    ymax="end", 
+                    fill="increase"
+                ),
+                size=0.5
+            )
+            + gg.geom_hline(yintercept=base_value, linetype="dotted")
+            + gg.geom_label(
                 aes(
                     x="x_id",
                     y=max_shap + 0.01,
@@ -321,18 +371,19 @@ def _(aes, c, gg, ggplot, ml, shot_shap):
                 labels=shot_shap["variable"].to_list(),
                 limits=(0.25, None)
             ) + gg.scale_y_continuous(
-                limits=(0, max_shap + 0.02),
+                limits=(None, max_shap + 0.02),
                 labels=ml.label_percent()
             )
-            + gg.scale_fill_manual(values={True: "#ff0051", False: "#008bfb"}) # Standard SHAP colors
-            + gg.scale_color_manual(values={True: "#ff0051", False: "#008bfb"}) # Standard SHAP colors
+            + gg.scale_fill_manual(values={True: "green", False: "red"}) # Standard SHAP colors
+            + gg.scale_color_manual(values={True: "green", False: "red"}) # Standard SHAP colors
             + gg.theme_538(base_size=8)
             + gg.coord_flip()
             + gg.theme(
                 panel_grid_major_y=gg.element_blank(),
-                panel_border=gg.element_rect(),
+                panel_border=gg.element_rect(size=0.5),
                 legend_position="none",
                 dpi=300,
+                axis_text_y=gg.element_text(weight='bold')
             )
             + gg.labs(
                 x="", y="Post-Shot xG"
