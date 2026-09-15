ee72a005ed5f54292ccc9ffaa2074262b9e22136
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2026-09-15 10:21:54 +0000

    Use setup cell

--- notebooks/apps/shot_dashboard.py
+++ notebooks/apps/shot_dashboard.py
@@ -1,11 +1,9 @@
 import marimo
 
 __generated_with = "0.24.2"
-app = marimo.App(width="medium")
+app = marimo.App(width="medium", app_title="Shot Dashboard")
 
-
-@app.cell
-def _():
+with app.setup:
     from datetime import date
     import time
 
@@ -18,34 +16,12 @@ def _():
     import mizani.labels as ml
 
     from data_readers import read_game_id_mapping, batch_read_shot_data, batch_read_entity_tracking, batch_read_puck_tracking, read_player_id_mapping
-    from plotting.rink import geom_ice, geom_net, geom_rink
+    from plotting.rink import geom_net, geom_rink
     from processing.tracking import calculate_elapsed_time, adjust_vectors
-
-    return (
-        adjust_vectors,
-        aes,
-        batch_read_entity_tracking,
-        batch_read_puck_tracking,
-        batch_read_shot_data,
-        c,
-        calculate_elapsed_time,
-        cs,
-        date,
-        geom_net,
-        geom_rink,
-        gg,
-        ggplot,
-        ml,
-        mo,
-        pl,
-        read_game_id_mapping,
-        read_player_id_mapping,
-        time,
-    )
 
 
 @app.cell
-def _(c, date, pl, read_game_id_mapping, read_player_id_mapping):
+def _():
     games = (
         read_game_id_mapping("mappings/NHL_20242025_20252026_game_smt_sportlogiq_id_map.csv")
         .with_columns(
@@ -58,34 +34,21 @@ def _(c, date, pl, read_game_id_mapping, read_player_id_mapping):
 
 
 @app.cell
-def _(c, games, mo):
+def _(games):
     game_strings = games.select(c('game_string')).to_series()
     game_selector = mo.ui.dropdown(options=game_strings, searchable=True, label="Game: ", allow_select_none=False, value=game_strings.first())
     return (game_selector,)
 
 
 @app.cell
-def _(c, game_selector, games):
+def _(game_selector, games):
     sportlogiq_id = games.filter(c('game_string') == game_selector.value).select(c('SportlogiqGameID')).item()
     smt_id = games.filter(c('game_string') == game_selector.value).select(c('SMTGameID')).item()
     return smt_id, sportlogiq_id
 
 
-@app.cell
-def _(
-    adjust_vectors,
-    batch_read_entity_tracking,
-    batch_read_puck_tracking,
-    batch_read_shot_data,
-    c,
-    calculate_elapsed_time,
-    cs,
-    games,
-    pl,
-    player_mapping,
-    smt_id,
-    sportlogiq_id,
-):
+@app.cell(hide_code=True)
+def _(games, player_mapping, smt_id, sportlogiq_id):
     shot_data = batch_read_shot_data(f"/output/shot_data/*-wide-window/{sportlogiq_id}_shot_data.parquet").with_row_index(offset=1).collect()
 
     pre_shot_xg = pl.scan_parquet("/output/predictions/*/pre_shot_0809.parquet").filter(c('game_id') == str(sportlogiq_id)).collect()
@@ -133,7 +96,7 @@ def _(
                 .otherwise(pl.lit('Missed'))
             )
         ).with_columns(
-            details = pl.format("{} {} - {}mph - {}", c('player_first_name'), c('player_last_name'), c('shot_speed').round(1), c('result'))
+            details = pl.format("{} {} - {}mph - {}", c('player_first_name'), c('player_last_name'), c('shot_speed').round(1).cast(pl.String).fill_null("N/A"), c('result'))
         )
         .with_columns(
             pl.concat_list(c('opposing_team_goalie_on_ice_ref')),
@@ -165,8 +128,8 @@ def _(
     return puck_tracking, shot_data
 
 
-@app.cell
-def _(c, cs, pl, sportlogiq_id):
+@app.cell(hide_code=True)
+def _(sportlogiq_id):
     shap = (
         pl.read_parquet(f"/output/shap/*/{sportlogiq_id}_shap.parquet")
         .unpivot(
@@ -189,14 +152,14 @@ def _(c, cs, pl, sportlogiq_id):
 
 
 @app.cell
-def _(c, mo, shot_data):
+def _(shot_data):
     max_index = shot_data.select(c('index').max()).item()
     shot_selector = mo.ui.number(start=1, stop=max_index, value=1, step=1)
     return (shot_selector,)
 
 
 @app.cell
-def _(c, pl, puck_tracking, shap, shot_data, shot_selector, time):
+def _(puck_tracking, shap, shot_data, shot_selector):
     shot_tracking = shot_data.filter(c('index') == shot_selector.value)
     shot_plot_data = shot_tracking.head(1)
 
@@ -238,7 +201,7 @@ def _(c, pl, puck_tracking, shap, shot_data, shot_selector, time):
 
 
 @app.cell
-def _(c, shot_plot_data):
+def _(shot_plot_data):
     pre_xg = shot_plot_data.select(c('pre_shot').round(2).first()).item()
     post_xg = shot_plot_data.select(c('post_shot').round(2).first()).item()
     if post_xg is None:
@@ -252,12 +215,8 @@ def _(c, shot_plot_data):
 
 @app.cell
 def _(
-    aes,
     details,
     game_selector,
-    geom_net,
-    gg,
-    ggplot,
     period,
     post_colour,
     post_xg,
@@ -293,27 +252,19 @@ def _(
 
 
 @app.cell
-def _(
-    aes,
-    geom_rink,
-    gg,
-    ggplot,
-    shot_plot_data,
-    shot_puck_tracking,
-    shot_tracking,
-):
+def _(shot_plot_data, shot_puck_tracking, shot_tracking):
     rink_plot = (
         ggplot(shot_plot_data, aes(x='shot_x', y='shot_y'))
         + geom_rink(fill='white')
         + gg.geom_point(
-            aes(x='x', y='y'), fill='grey', alpha=0.2, data=shot_puck_tracking
-        )
-        + gg.geom_point(
-            aes(x='x_adj', y='y_adj', fill='team_source'),
+            aes(x='x_adj', y='y_adj', fill='team_source', tooltip="PlayerName", hover_group="PlayerName"),
             size=8,
             data=shot_tracking,
-            inherit_aes=False)
-        + gg.geom_point()
+            inherit_aes=False
+        )
+        + gg.geom_point(
+            aes(x='x', y='y'), fill='grey', alpha=0.2, data=shot_puck_tracking
+        )
         + gg.geom_segment(aes(xend=89, yend='goalline_y'), size=0.8, arrow=gg.arrow(length=0.1, type='closed'))
         + gg.coord_fixed(xlim=(25,None))
         + gg.theme_538()
@@ -328,23 +279,14 @@ def _(
 
 
 @app.cell
-def _(aes, base_value, c, gg, ggplot, ml, shot_shap):
+def _(base_value, shot_shap):
     if shot_shap.height > 0:
         max_shap = shot_shap.select(c('end').max()).item()
 
         shap_plot = (
             ggplot(shot_shap)
-            + gg.geom_segment(
-                aes(
-                    x="x_id - 1.4", 
-                    xend="x_id + 0.4", 
-                    y="start", 
-                    yend="start"
-                ),
-                linetype="dashed", 
-                color="gray",
-                inherit_aes=False
-            )
+            + gg.geom_hline(yintercept=base_value, linetype="dotted")
+            + gg.geom_hline(yintercept=0, alpha=0.6)
             + gg.geom_rect(
                 aes(
                     xmin="x_id - 0.4", 
@@ -355,7 +297,17 @@ def _(aes, base_value, c, gg, ggplot, ml, shot_shap):
                 ),
                 size=0.5
             )
-            + gg.geom_hline(yintercept=base_value, linetype="dotted")
+            + gg.geom_segment(
+                aes(
+                    x="x_id - 1.4", 
+                    xend="x_id + 0.4", 
+                    y="start", 
+                    yend="start"
+                ),
+                # linetype="dashed", 
+                color="gray",
+                inherit_aes=False
+            )
             + gg.geom_label(
                 aes(
                     x="x_id",
@@ -395,7 +347,7 @@ def _(aes, base_value, c, gg, ggplot, ml, shot_shap):
 
 
 @app.cell
-def _(game_selector, goal_plot, mo, rink_plot, shap_plot, shot_selector):
+def _(game_selector, goal_plot, rink_plot, shap_plot, shot_selector):
     mo.vstack([
         mo.hstack([
             game_selector, shot_selector
